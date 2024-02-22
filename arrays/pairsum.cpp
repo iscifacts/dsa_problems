d455389c273786b0a25ac9d0ea0b07d2e2088140
unknown <aishita219@gmail.com>
2024-02-23 00:38:06 +0530

    update new file

--- arrays/pairsum.cpp
+++ arrays/pairsum.cpp
@@ -1,8 +1,11 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+you may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-// You can return the answer in any order.
+You can return the answer in any order.*/
+
+//name-Ishita Agrawal 
+//question no. 1
 #include<bits/stdc++.h>
 using namespace std;
  class Solution {
