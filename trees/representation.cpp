406d6c8b570f67201044d381451e649b29fa7b56
unknown <aishita219@gmail.com>
2024-03-02 19:28:33 +0530

    add new file

--- trees/representation.cpp
+++ trees/representation.cpp
@@ -11,11 +11,45 @@ struct Node{
         left=right=NULL;
     }
 };
+void preOrder(Node* root)
+{
+    if(root==NULL)
+    return ;
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
+void InOrder(Node* root)
+{
+    if(root==NULL)
+    return ;
+    preOrder(root->left);
+    cout<<root->data<<" ";
+    preOrder(root->right);
+    
+}
+void postOrder(Node* root)
+{
+    if(root==NULL)
+    return ;
+    preOrder(root->left);
+    preOrder(root->right);
+    cout<<root->data<<" ";
+}
 int main()
 {
     struct Node* root=new Node(1);
     root->left=new Node(2);
     root->right=new Node(3);
     root->left->right=new Node(4);
+    cout<<"PreOrder Traversal"<<endl;
+    preOrder(root);
+    cout<<endl;
+    cout<<"InOrder Traversal"<<endl;
+    InOrder(root);
+    cout<<endl;
+    cout<<"PostOrder Traversal"<<endl;
+    postOrder(root);
     return 0;
 }
